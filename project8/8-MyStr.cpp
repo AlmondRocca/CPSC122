204d9d0f710fe3d02c5be9c5194fd16e1708b27c
Kevin Rocca <abc@example.com>
2022-11-06 01:03:18 -0700

    Update Classes

---
+++ project8/8-MyStr.cpp
@@ -0,0 +1,124 @@
+/*
+Name: Kevin Rocca
+Class: CPSC 122, Project 8
+10/27/22
+This progam is an implementation of the functions in 8-MyStr.h
+*/
+#include <iostream>
+using namespace std;
+
+#include "8-MyStr.h"
+#include <cstring>
+
+MyString::MyString(char const* strIn)
+{
+ length = strlen(strIn);  //strlen function
+ str = new char[length + 1];
+ strcpy(str, strIn); //strlen function
+
+}
+
+MyString::~MyString()
+{
+ delete []str; 
+}
+
+void MyString::myDisplay()
+{
+ cout << str << endl;
+}
+
+int MyString::myStrLen(char const* strIn)
+{
+ int idx = 0;
+ while (strIn[idx] != '\0')
+  idx++;
+ return idx;
+}
+
+int MyString::myStrlen()
+{
+ return myStrLen(str);
+}
+
+
+void MyString::myStrcpy(char const* strIn){
+    int newLength = myStrLen(strIn);
+    delete []str;
+
+    length = newLength;
+    str = new char[length];
+
+    for(int i = 0; i < length; i++){
+        str[i] = strIn[i];
+    }
+}
+
+
+bool MyString::isEqual(char const* strIn){
+    int idx = 0;
+    while(strIn[idx] != '\0')
+    {
+        if(strIn[idx] != str[idx]){
+            return false;
+        }
+        idx++;
+    }
+    if(idx != length){
+        return false;
+    }
+    return true;
+}
+
+
+int MyString::find(char const* strIn){
+    int returnPos = -1;
+    int idx = 0;
+
+    while(idx < length){
+        if(strIn[0] == str[idx]){
+            if(isSub(strIn, idx)){
+                returnPos = idx;
+                break;
+            }
+        }
+        idx++;
+    }
+    return returnPos;
+}
+
+
+bool MyString::isSub(char const* strIn, int idx){
+    int cur = 0;
+    for(int i = idx; i < length; i++){
+        if(strIn[cur] == '\0'){
+            break;
+        }
+        if(!(strIn[cur] == str[i])){
+            return false;
+        }
+        cur++;
+    }
+    return true;
+}
+
+
+void MyString::concat(char const* strIn){
+    int idx = 0;
+    int newLength = length + myStrLen(strIn);
+    char* newStr = new char[newLength + 1];
+    length = newLength;
+
+    while(str[idx] != '\0'){
+        newStr[idx] = str[idx];
+        idx++;
+    }
+    delete []str;
+    int i = 0;
+    while(strIn[i] != '\0'){
+        newStr[idx] = strIn[i];
+        idx++;
+        i++;
+    }
+    str = newStr;
+    }
---
+++ project8/8-MyStrMain.cpp
@@ -0,0 +1,91 @@
+/*
+Name: Paul De Palma
+Class: CPSC 122, Section 1
+Date Submitted: February 18, 2021
+Assignment: N/A 
+Description: Program illustrates using class MyString 
+*/
+
+#include "8-MyStr.h"
+#include <iostream> 
+using namespace std;
+
+
+int main(int argc, char* argv[])
+{
+ MyString str1(argv[1]);
+ MyString* str2 = new MyString(argv[1]);
+
+/*
+ //Test of myDisplay
+ cout << "*****Test myDisplay*****" << endl;
+ cout << "static test" << endl;
+ cout << "output should be the command line input" << endl;
+ str1.myDisplay();
+ cout << endl;
+ //End Test of myDisplay
+*/
+
+/*
+ //Test of isEqual
+ cout << "*****Test isEqual*****" << endl;
+ cout << "static test" << endl;
+ char const* test = "hi there";
+ bool test2 = str1.isEqual(test);
+ if(test2 == 0){
+  cout << "false" << endl;
+ }
+ else{
+    cout << "true" << endl;
+ }
+ //End Test of isEqual
+*/
+
+/*
+ //Test of myStrcpy
+ cout << "*****Test myStrcpy*****" << endl;
+ cout << "static test" << endl;
+ cout << "output should be whatever is in test below" << endl;
+ char const* test = "replaced?";
+ str1.myDisplay();         
+ str1.myStrcpy(test);
+ str1.myDisplay();
+ //End Test of myStrcpy
+*/
+
+/*
+ //Test of myStrlen
+ cout << "*****Test myStrlen*****" << endl;
+ cout << "dynamic test" << endl;
+ cout << "output should be length of command line input" << endl;
+ cout << str2->myStrlen() << endl;
+ cout << endl;
+ //End Test of myStrlen
+*/
+
+/*
+ //Test of find and isSub since they work together.
+ cout << "*****Test find and isSub*****" << endl;
+ cout << "static test" << endl;
+ cout << "output should be the starting position of the substring" << endl;
+ cout << "finding 'low' in 'swallow' should return 4" << endl;
+ cout << str1.find("low") << endl;
+ //End Test of find and isSub
+*/
+
+/*
+ //Test of concat
+ cout << "*****Test concat*****" << endl;
+ cout << "dynamic test" << endl;
+ cout << "output should be command input and whatever is in here" << endl;
+ str2->myDisplay();
+ str2->concat("added on");
+ str2->myDisplay();
+ //End Test of concat
+*/
+
+ delete str2;
+ return 0;  
+}
+   
+  
